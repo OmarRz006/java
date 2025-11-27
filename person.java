9a25c739af0bd3513850ebb9548aa7f9545d10f9
OmarRz006 <omerrzaq@gmail.com>
2025-11-27 14:43:36 +0300

    Add files via upload

---
+++ person.java
@@ -0,0 +1,12 @@
+
+public class person {
+    String name;
+    int age;
+    
+    
+    public void talk(){
+        System.out.println("Hi, my name is "+name);
+        
+}
+}
+
---
+++ persontest.java
@@ -0,0 +1,15 @@
+public class persontest {
+  public static void main(String[] args) {
+   person p1=new person();
+   p1.name="ata";
+   p1.age=19;
+   p1.talk();
+   person p2=new person();
+   p2.name="Essa";
+   p2.age=20;
+   p2.talk();
+   
+}
+  
+    
+}
